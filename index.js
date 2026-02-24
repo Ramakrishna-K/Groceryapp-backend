97f469dda0a01cec898b33f51dfe188b66899467
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-24 18:46:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import { connectDB } from "./config/connectDB.js";
 dotenv.config();
+// import userRoutes from "./routes/userRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import sellerRoutes from "./routes/seller.routes.js";
 import productRoutes from "./routes/product.routes.js";
