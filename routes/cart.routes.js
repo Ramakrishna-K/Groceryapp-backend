481e2c3e041f4f0349c820689a58f7602fa0feef
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-25 07:38:00 +0530

    Update cart.routes.js

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,21 +1,10 @@
-// import express from "express";
-// import { updateCart } from "../controller/cart.controller.js";
-// import authUser from "../middlewares/authUser.js";
-
-// const router = express.Router();
-// router.put("/update", authUser, updateCart);
-// export default router;
-
-
-
 import express from "express";
-// Double check: Is your folder "controllers" or "controller"?
-import { updateCart } from "../controllers/cart.controller.js"; 
-// Double check: Is your folder "middleware" or "middlewares"?
-import authUser from "../middleware/authUser.js"; 
+import { updateCart } from "../controller/cart.controller.js";
+import authUser from "../middlewares/authUser.js";
 
-const cartRouter = express.Router();
+const router = express.Router();
+router.put("/update", authUser, updateCart);
+export default router;
 
-cartRouter.put("/update", authUser, updateCart);
 
-export default cartRouter;
+
