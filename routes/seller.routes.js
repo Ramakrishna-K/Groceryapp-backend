f82c43efbafeaae6b5ec048fb02ff5c6083241e3
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-25 18:35:14 +0530

    Update seller.routes.js

--- routes/seller.routes.js
+++ routes/seller.routes.js
@@ -1,30 +1,30 @@
-// import express from "express";
-// import {
-//   checkAuth,
-//   sellerLogin,
-//   sellerLogout,
-// } from "../controller/seller.controller.js";
-// import { authSeller } from "../middlewares/authSeller.js";
-// const router = express.Router();
-
-// router.post("/login", sellerLogin);
-// router.get("/is-auth", authSeller, checkAuth);
-// router.get("/logout", authSeller, sellerLogout);
-
-// export default router;
-
-
-
-// routes/seller.routes.js
 import express from "express";
-import { sellerLogin, checkAuth, sellerLogout } from "../controller/seller.controller.js";
-import { sellerAuth } from "../middleware/sellerAuth.js";
-
+import {
+  checkAuth,
+  sellerLogin,
+  sellerLogout,
+} from "../controller/seller.controller.js";
+import { authSeller } from "../middlewares/authSeller.js";
 const router = express.Router();
 
 router.post("/login", sellerLogin);
-router.get("/is-auth", sellerAuth, checkAuth);
-router.get("/logout", sellerLogout);
+router.get("/is-auth", authSeller, checkAuth);
+router.get("/logout", authSeller, sellerLogout);
 
 export default router;
+
+
+
+// // routes/seller.routes.js
+// import express from "express";
+// import { sellerLogin, checkAuth, sellerLogout } from "../controller/seller.controller.js";
+// import { sellerAuth } from "../middleware/sellerAuth.js";
+
+// const router = express.Router();
+
+// router.post("/login", sellerLogin);
+// router.get("/is-auth", sellerAuth, checkAuth);
+// router.get("/logout", sellerLogout);
+
+// export default router;
 
