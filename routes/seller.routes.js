5729edc8851d51016009323f4635c3f00c4703c5
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-25 17:30:15 +0530

    Update seller.routes.js

--- routes/seller.routes.js
+++ routes/seller.routes.js
@@ -1,15 +1,39 @@
+// import express from "express";
+// import {
+//   checkAuth,
+//   sellerLogin,
+//   sellerLogout,
+// } from "../controller/seller.controller.js";
+// import { authSeller } from "../middlewares/authSeller.js";
+// const router = express.Router();
+
+// router.post("/login", sellerLogin);
+// router.get("/is-auth", authSeller, checkAuth);
+// router.get("/logout", authSeller, sellerLogout);
+
+// export default router;
+
+// routes/seller.routes.js
 import express from "express";
 import {
-  checkAuth,
   sellerLogin,
   sellerLogout,
+  checkAuth,
 } from "../controller/seller.controller.js";
 import { authSeller } from "../middlewares/authSeller.js";
+
 const router = express.Router();
 
+// seller login
 router.post("/login", sellerLogin);
+
+// check seller auth
 router.get("/is-auth", authSeller, checkAuth);
+
+// seller logout
 router.get("/logout", authSeller, sellerLogout);
 
 export default router;
+
+
 
