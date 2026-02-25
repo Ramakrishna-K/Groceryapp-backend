c8746e80ca0ac604d4407423ca2a5d2260d0ab4d
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-25 07:42:25 +0530

    Update cart.routes.js

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,10 +1,22 @@
+// import express from "express";
+// import { updateCart } from "../controller/cart.controller.js";
+// import authUser from "../middlewares/authUser.js";
+
+// const router = express.Router();
+// router.put("/update", authUser, updateCart);
+// export default router;
+
+
 import express from "express";
-import { updateCart } from "../controller/cart.controller.js";
-import authUser from "../middlewares/authUser.js";
+// Ensure your folder is actually named 'controller'
+import { updateCart } from "../controller/cart.controller.js"; 
+// Ensure your folder is actually named 'middlewares'
+import authUser from "../middlewares/authUser.js"; 
 
-const router = express.Router();
-router.put("/update", authUser, updateCart);
-export default router;
+const cartRouter = express.Router();
 
+// This route uses the authUser middleware to get the userId from the cookie
+// and then calls updateCart to update the database.
+cartRouter.put("/update", authUser, updateCart);
 
-
+export default cartRouter;
