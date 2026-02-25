6232a8d1c3eb1096d63b496140fa40b0c948ee53
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-25 07:29:13 +0530

    Update cart.routes.js

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,7 +1,25 @@
-import express from "express";
-import { updateCart } from "../controller/cart.controller.js";
-import authUser from "../middlewares/authUser.js";
+// import express from "express";
+// import { updateCart } from "../controller/cart.controller.js";
+// import authUser from "../middlewares/authUser.js";
 
-const router = express.Router();
-router.put("/update", authUser, updateCart);
-export default router;
+// const router = express.Router();
+// router.put("/update", authUser, updateCart);
+// export default router;
+
+// cart.routes.js
+import express from "express";
+import { updateCart } from "../controllers/cart.controller.js";
+import authUser from "../middleware/authUser.js";
+
+const cartRouter = express.Router();
+
+/**
+ * Route: PUT /api/cart/update
+ * Middleware: authUser (extracts token from cookie and adds userId to req.body)
+ * Controller: updateCart (handles the database logic)
+ */
+cartRouter.put("/update", authUser, updateCart);
+
+export default cartRouter;
+
+
