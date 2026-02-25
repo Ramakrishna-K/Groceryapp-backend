fcd2338139dc46fd05c3150975e1e066a3104ac9
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-25 06:23:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import { connectDB } from "./config/connectDB.js";
 dotenv.config();
-// import userRoutes from "./routes/userRoutes.js";
+// import userRoutes from "./routes/user.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import sellerRoutes from "./routes/seller.routes.js";
@@ -154,51 +154,4 @@ app.listen(PORT, () => {
 
 
 
-
-
-// import express from "express";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import dotenv from "dotenv";
-// import { connectDB } from "./config/connectDB.js";
-// dotenv.config();
-// import userRoutes from "./routes/user.routes.js";
-// import sellerRoutes from "./routes/seller.routes.js";
-// import productRoutes from "./routes/product.routes.js";
-// import cartRoutes from "./routes/cart.routes.js";
-// import addressRoutes from "./routes/address.routes.js";
-// import orderRoutes from "./routes/order.routes.js";
-
-// import { connectCloudinary } from "./config/cloudinary.js";
-
-// const app = express();
-
-// await connectCloudinary();
-
-// // ✅ Allow multiple origins including Vercel frontend
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "https://groceryapp-frontend-rosy.vercel.app"
-// ];
-
-// // Middlewares
-// app.use(cors({ origin: allowedOrigins, credentials: true }));
-// app.use(cookieParser());
-// app.use(express.json());
-
-// // Api endpoints
-// app.use("/images", express.static("uploads"));
-// app.use("/api/user", userRoutes);
-// app.use("/api/seller", sellerRoutes);
-// app.use("/api/product", productRoutes);
-// app.use("/api/cart", cartRoutes);
-// app.use("/api/address", addressRoutes);
-// app.use("/api/order", orderRoutes);
-
-// // Server listen
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   connectDB();
-//   console.log(`Server is running on port ${PORT}`);
-// });
 
