468ab532d202ce3e4b008746df4640d8a76c977a
Ramakrishna.K <ramakrishnakatravath0@gmail.com>
2026-02-25 17:40:32 +0530

    Update seller.model.js

--- models/seller.model.js
+++ models/seller.model.js
@@ -1,1 +1,27 @@
 
+import mongoose from "mongoose";
+
+const sellerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Seller = mongoose.model("Seller", sellerSchema);
+
+export default Seller;
